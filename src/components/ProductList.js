d9b268e782ed343f8932a4f0f9f0dfdb21a46c90
Nikita R <rdnkta@gmail.com>
2019-04-20 21:52:55 +0300

    fix small bugs

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import ProductItem from './ProductItem';
 import Footer from './Footer';
 
 export default class ProductList extends Component {
@@ -9,156 +10,17 @@ export default class ProductList extends Component {
 				<Header shift={this.props.shift} />
 				<main>
 					<section className="product-list u-mt-huge">
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>{' '}
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>{' '}
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>{' '}
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>{' '}
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>{' '}
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>{' '}
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>{' '}
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>{' '}
-						<div className="product">
-							<div className="product__image-wrapper">
-								<img
-									className="product__image"
-									src="img/product-min.png"
-									alt="Product"
-								/>
-							</div>
-							<div className="product__info">
-								<span className="product__title">Product Title</span>
-								<span className="product__btn">
-									<img src="img/icons/fa-eye.png" alt="Eye" />
-								</span>
-							</div>
-						</div>
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
+						<ProductItem />
 					</section>
 				</main>
 				<Footer />
