5d188c4a63c013adfb34216ddb623677bfaefe0b
Nick R <rdnkta@gmail.com>
2019-04-30 13:49:10 +0300

    get posters

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './ProductItem.module.scss';
 
-export default function ProductItem() {
-	return (
-		<div className={styles.product}>
-			<div className={styles.imageWrapper}>
-				<img className={styles.image} src="img/product-min.png" alt="Product" />
-			</div>
-			<div className={styles.info}>
-				<span className={styles.title}>Product Title</span>
-				<span className={styles.btn}>
-					<img src="img/icons/fa-eye.png" alt="Eye" />
-				</span>
-			</div>
-		</div>
-	);
+export default function ProductItem(props) {
+  const { theme, images } = props.item;
+
+  return (
+    <div className={styles.product}>
+      <div className={styles.imageWrapper}>
+        <img
+          className={styles.image}
+          src={images.length === 0 ? 'img/default.jpg' : images[0].file}
+          alt='Product'
+        />
+      </div>
+      <div className={styles.info}>
+        <span className={styles.title}>{theme ? theme : 'Product Title'}</span>
+        <span className={styles.btn}>
+          <img src='img/icons/fa-eye.png' alt='Eye' />
+        </span>
+      </div>
+    </div>
+  );
 }
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../Header/Header';
 import ProductItem from '../ProductItem/ProductItem';
 import Footer from '../Footer/Footer';
 import styles from './ProductList.module.scss';
 
-export default function ProductList() {
-	return (
-		<>
-			<Header />
-			<main>
-				<section className={styles.productList}>
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-					<ProductItem />
-				</section>
-			</main>
-			<Footer />
-		</>
-	);
+export default class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://light-it-04.tk/api/posters/')
+      .then(res => res.json())
+      .then(obj =>
+        this.setState({
+          data: obj.data
+        })
+      );
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <>
+        <Header />
+        <main>
+          <section className={styles.productList}>
+            {data
+              ? data.map(item => {
+                  return <ProductItem key={item.pk} item={item} />;
+                })
+              : 'Loading...'}
+          </section>
+        </main>
+        <Footer />
+      </>
+    );
+  }
 }
